deda0597246ad34118bc2368e02ce1e826798568
Shawn <schlosser.shawn@gmail.com>
2016-04-23 21:44:20 -0500

    simplifying app.js

--- app.js
+++ app.js
@@ -4,11 +4,4 @@ angular.module('app', [
   'ngRoute',
   'ui.bootstrap',
   'ngAnimate',
-  // 'app.global',
-  // 'app.dashboard'
-])
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({
-    redirectTo: '/dashboard'
-  });
-}]);
+]);
